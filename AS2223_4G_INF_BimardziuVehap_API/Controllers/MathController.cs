d929935cb8810b7aa3af03da9fe9e186eaa16db8
VehapBunardziu <vehap.bunardziu@studenti.ittsrimini.edu.it>
2023-03-16 08:19:22 +0100

    feat: Added HttpGet evenorodd, that given a number in input returns if it's even or odd

---
+++ AS2223_4G_INF_BimardziuVehap_API/Controllers/MathController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_BimardziuVehap_API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1/")]
+    public class MathController : Controller
+    {
+        [HttpGet("evenorodd")]
+        public JsonResult Evenorodd(int n)
+        {
+            if (n % 2 == 0)
+            { 
+                return Json(new { output = new {number = n, message = "even"}, status = "OK" });
+
+            }else if (n % 2 == 1)
+            {
+                return Json(new { output = new { number = n, message = "odd" }, status = "OK" });
+            }
+            else
+            {
+                return Json(new { output = new { number = n, message = "" }, status = "KO" });
+            }
+           
+        }
+    }
+}
