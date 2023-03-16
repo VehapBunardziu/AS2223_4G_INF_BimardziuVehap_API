619fb3aeae1fd673e0a270b87f057e3f519beca2
VehapBunardziu <vehap.bunardziu@studenti.ittsrimini.edu.it>
2023-03-16 08:47:51 +0100

    feat: Added HttpGet Factorial that given a number it gives the factorial.

--- AS2223_4G_INF_BimardziuVehap_API/Controllers/MathController.cs
+++ AS2223_4G_INF_BimardziuVehap_API/Controllers/MathController.cs
@@ -24,5 +24,41 @@ namespace AS2223_4G_INF_BimardziuVehap_API.Controllers
             }
            
         }
+
+
+        [HttpGet("factorial")]
+        public JsonResult Factorial(int n)
+        {
+            if(n >= 0)
+            {
+                int output = n; 
+                for(int i = n; i > 1; i--)
+                {
+                    output = output + (output * i-1);
+                }
+                return Json(new { output = new { number = n, factorial = output}, status = "OK" });
+            }
+            else
+            {
+                return Json(new { output = new { number = n, message = "Number is negative" }, status = "KO" });
+            }
+
+        }
+
+        [HttpGet("Income-basedtaxes")]
+        public JsonResult Incomebasedtaxes(int income)
+        {
+            if(income < 35000)
+            {
+                int temp = (income / 100) * 12;
+                return Json(new { output = new { income = income, taxes = temp}, status = "OK" });
+            }
+            else
+            {      
+                int temp = (35000/ 100) * 12;
+                temp = temp + (((income - 35000) / 100) * 28);
+                return Json(new { output = new { income = income, taxes = temp }, status = "OK" });
+            }
+        }
     }
 }
